bd6e66182fe5d710ba29daf4dcbf326d12a1b218
McKayla Washburn <mckayla@hey.com>
2023-07-12 15:38:44 -0600

    `tabWidth: 2`

--- packages/prettier/index.js
+++ packages/prettier/index.js
@@ -1,13 +1,7 @@
 module.exports = {
-	printWidth: 90,
-	quoteProps: "consistent",
-	tabWidth: 4, // Necessary for prettier to align things, even though we use tabs
-	trailingComma: "all",
-	useTabs: true,
-	overrides: [
-		{
-			files: ["./**/*.yml", "./**/*.yaml"],
-			options: { tabWidth: 2 },
-		},
-	],
+  printWidth: 90,
+  quoteProps: "consistent",
+  tabWidth: 2, // Necessary for prettier to align things, even though we use tabs
+  trailingComma: "all",
+  useTabs: true,
 };
