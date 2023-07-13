1f8e02fa2d926a38f63ccb1060915f7326300e7b
McKayla Washburn <mckayla@hey.com>
2023-07-13 09:43:14 -0600

    `proseWrap: "never"`

--- packages/prettier/index.js
+++ packages/prettier/index.js
@@ -1,5 +1,6 @@
 module.exports = {
   printWidth: 90,
+  proseWrap: "never",
   quoteProps: "consistent",
   tabWidth: 2, // Necessary for prettier to align things, even though we use tabs
   trailingComma: "all",
